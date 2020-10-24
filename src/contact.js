094591b6bf73d5b3a702a272dcca51d8366e649d
shibili <shibilikkv@gmail.com>
2020-10-24 19:27:22 +0530

    function component

---
+++ src/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+function Contact(){
+    return(
+        <div>
+            <label>Email id</label>
+            <input type="text" />
+            <br/>
+            <label>Message</label>
+            <textarea/>
+            <button>Send</button>
+        </div>
+    )
+}
+
+export default Contact ;
